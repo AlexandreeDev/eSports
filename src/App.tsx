7e89c90d9f0527382258bc9af3ab038907b69fe5
AlexandreeDev <Alexandree999@hotmail.com>
2024-03-04 17:15:25 -0300

    finalization

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import "./styles/main.css";
 import { useState, useEffect } from "react";
 
 import * as Dialog from "@radix-ui/react-dialog";
-import { GameController } from "phosphor-react";
+import axios from "axios";
 
 import logoImg from "./assets/logo-nlw-esports.svg";
 import { CreateAdBanner } from "./components/CreateAdBanner";
+import { CreateAdModal } from "./components/Form/CreateAdModal";
 import { GameBanner } from "./components/GameBanner";
 
 interface Game {
@@ -21,11 +22,9 @@ function App() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/games")
-      .then((response) => response.json())
-      .then((data) => {
-        setGames(data);
-      });
+    axios("http://localhost:3000/games").then((response) => {
+      setGames(response.data);
+    });
   }, []);
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
@@ -51,73 +50,7 @@ function App() {
       </div>
       <Dialog.Root>
         <CreateAdBanner />
-        <Dialog.Portal>
-          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-black/25">
-            <Dialog.Title className="text-3xl font-black">
-              Publique um anúncio
-            </Dialog.Title>
-            <Dialog.Content>
-              <form>
-                <div>
-                  <label htmlFor="game">Qual o game?</label>
-                  <input
-                    id="game"
-                    placeholder="Selecione o game que deseja jogar"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="name">Seu nome (ou nick name)</label>
-                  <input
-                    id="name"
-                    placeholder="Como te chamam dentro do game?"
-                  />
-                </div>
-                <div>
-                  <div>
-                    <label htmlFor="yearsPlaying">Joga a quantos anos?</label>
-                    <input
-                      id="yearsPlaying"
-                      type="number"
-                      placeholder="Tudo bem ser ZERO"
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="discord">Qual o seu discord?</label>
-                    <input
-                      id="discord"
-                      type="text"
-                      placeholder="Usuario#0000"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div>
-                    <label htmlFor="weekDays">Quando costuma jogar?</label>
-                  </div>
-                  <div>
-                    <label htmlFor="hourStart">Qual o horário do dia?</label>
-                    <div>
-                      <input id="hourStart" type="time" placeholder="De" />
-                      <input id="hourEnd" type="time" placeholder="Até" />
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <input type="checkbox" />
-                  Costumo me conectar ao chat de voz
-                </div>
-                <footer>
-                  <button>Cancelar</button>
-                  <button type="submit">
-                    <GameController />
-                    Encontrar duo
-                  </button>
-                </footer>
-              </form>
-            </Dialog.Content>
-          </Dialog.Content>
-        </Dialog.Portal>
+        <CreateAdModal />
       </Dialog.Root>
     </div>
   );
